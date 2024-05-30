a83c94174c999f27e96217588128f660e3fe0516
Praveenragam <praveenteja.ragam1@gmail.com>
2024-05-30 17:41:31 +0530

    first

---
+++ src/Mainpage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+function Mainpage() {
+  return (
+    <div>
+      <Header/>
+      <div><img src="https://rb.gy/xy3ho2" alt="React Image" />
+</div> 
+   <p>Officia in nisi laboris cillum laborum aute ullamco do excepteur sit proident ut
+         irure fugiat. Aute fugiat deserunt mollit aute anim. Sunt duis cupidatat dolor
+          cillum magna tempor incididunt. Ea magna laboris ut nostrud elit exercitation esse.
+           Minim proident sint incididunt Lorem nostrud tempor proident aute excepteur nulla
+            amet voluptate ad. Ad reprehenderit enim voluptate in aute et laborum dolore
+             veniam enim magna in aliqua aute. In occaecat culpa non et aliquip magna officia
+              proident aute aliquip.</p>
+
+      <Footer/>
+    </div>
+  )
+}
+
+export default Mainpage
